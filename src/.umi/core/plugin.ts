d8c66b6a489e0d83a8e356ed6091bb4c23aa388e
EnzoChan <chen.kunfeng@iwhalecloud.com>
2022-11-01 17:38:51 +0800

    pages

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/kunfengchen/Documents/as_pro/clockPro/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','request','mobileLayout',],
-});
-
-export { plugin };
